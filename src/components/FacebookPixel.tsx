21cc8b02cdeea7fc69d1f8aeeffe9ec0dba82be3
willy <zhmsistemas@gmail.com>
2026-07-16 19:56:54 -0500

    agrega publicidad facebook

--- src/components/FacebookPixel.tsx
+++ src/components/FacebookPixel.tsx
@@ -10,8 +10,15 @@ export default function FacebookPixel() {
         strategy="afterInteractive"
       >
         {`
-          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1420429106574309');
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1699331981302260');
           fbq('track', 'PageView');
         `}
       </Script>
@@ -19,7 +26,8 @@ export default function FacebookPixel() {
         <img
           height="1"
           width="1"
-          src="https://www.facebook.com/tr?id=1420429106574309&ev=PageView&noscript=1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1699331981302260&ev=PageView&noscript=1"
         />
       </noscript>
     </>
